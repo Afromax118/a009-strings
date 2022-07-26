2f98bbc02b8801ab75f1d6c45e4cae2e9a4da585
Gabriel Max <gabrielmaxcerqueira@gmail.com>
2022-07-26 08:22:57 -0300

    Exercicio de fixação A009

---
+++ exercicio1/script.js
@@ -0,0 +1,12 @@
+const nome = prompt("Qual seu nome?");
+
+const food1 = prompt("Qual sua primeira comida favorita?");
+const food2 = prompt("Qual sua segunda comida favorita?");
+const food3 = prompt("Qual sua terceira comida favorita?");
+
+
+const ascomidas = ("Estas são as comidas do ")
+
+const nomeComidas = `Estão são as comidas favoritas do(a) ${nome}:\n${food1}\n${food2}\n${food3}`
+
+console.log(nomeComidas)
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,18 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+console.log(minhaString.length);
+const minhaString1 = minhaString.trim();
+console.log(minhaString1.length);
+
+
+const quantidadeespaço = minhaString.length
+
+const quantidadeespaço1 = minhaString1.length
+
+console.log(`A frase ${minhaString} possui ${minhaString.length} caracteres \nA frase ${minhaString} possui ${minhaString1.length} caracteres`)
+
+const semTraco = minhaString1.replace("______","sticioso");
+
+console.log (semTraco)
+
+
+
